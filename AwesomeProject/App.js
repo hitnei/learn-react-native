7930f6eea9076a70fd66ac85bc4ae8d55ba1f38c
hitnei <hoangnhi050298@gmail.com>
2020-12-05 10:27:51 +0700

    update style but not work

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,26 +1,6 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  flex3: {
-    flex: 3,
-  },
-  flex2: {
-    flex: 2,
-  },
-  flex1: {
-    flex: 1,
-  },
-  marginBottom: {
-    marginBottom: 30,
-  },
-});
-
 // const HelloWorld = (props) => {
 //   return (
 //     <View>
@@ -51,12 +31,46 @@ class App extends Component {
       <View style={[styles.center]}>
         <Text>{this.state.count}</Text>
         <View>
-          <Button onPress={this.inscCount} title="insc" />
-          <Button onPress={this.descCount} title="desc" />
+          <Button
+            style={[styles.button]}
+            onPress={this.inscCount}
+            title="insc"
+          />
+          <Button
+            style={[styles.button]}
+            onPress={this.descCount}
+            title="desc"
+          />
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flex3: {
+    flex: 3,
+  },
+  flex2: {
+    flex: 2,
+  },
+  flex1: {
+    flex: 1,
+  },
+  marginBottom: {
+    marginBottom: 30,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
 
 export default App;
