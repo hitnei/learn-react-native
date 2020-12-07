3acb84cfc1ae1cdb7b50be29bfda0802a6e3fdbc
hitnei <hoangnhi050298@gmail.com>
2020-12-07 20:33:25 +0700

    feed cat

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,76 +1,31 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import React, {Component, useState} from 'react';
+import {Text, View, Button} from 'react-native';
 
-// const HelloWorld = (props) => {
-//   return (
-//     <View>
-//       <Text>Hello {props.name}</Text>
-//     </View>
-//   );
-// };
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
+  return (
+    <View>
+      <Text>My name is {props.name}</Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? 'Feel Cat' : 'Full'}
+      />
+    </View>
+  );
+};
 
 class App extends Component {
-  state = {
-    count: 0,
-  };
-
-  inscCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  descCount = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
-
   render() {
     return (
-      <View style={[styles.center]}>
-        <Text>{this.state.count}</Text>
-        <View>
-          <Button
-            style={[styles.button]}
-            onPress={this.inscCount}
-            title="insc"
-          />
-          <Button
-            style={[styles.button]}
-            onPress={this.descCount}
-            title="desc"
-          />
-        </View>
+      <View>
+        <Cat name="Bob" />
+        <Cat name="Vui" />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  center: {
-    flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  flex3: {
-    flex: 3,
-  },
-  flex2: {
-    flex: 2,
-  },
-  flex1: {
-    flex: 1,
-  },
-  marginBottom: {
-    marginBottom: 30,
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
-  },
-});
 
 export default App;
