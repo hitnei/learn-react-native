eee4743c4117ee4c5909b3972e62541ec062c55c
hitnei <hoangnhi050298@gmail.com>
2020-12-08 21:25:25 +0700

    scroll view

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,26 +1,47 @@
-import React, {useState} from 'react';
-import {View, TextInput, Text} from 'react-native';
+import React from 'react';
+import {ScrollView, Image, Text} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
 
 const App = () => {
-  var [text, setText] = useState('');
-
-  const onHandleChangeText = (textInput) => {
-    setText('🍕'.repeat(textInput.trim().split(' ').length));
-  };
-
   return (
-    <View style={{padding: 30}}>
-      <TextInput
-        placeholder="Enter text"
-        onChangeText={(text) => onHandleChangeText(text)}
-        style={{
-          borderBottomWidth: 1,
-          paddingBottom: 1,
-          borderBottomColor: '#cccccc',
-        }}
-      />
-      <Text>{text}</Text>
-    </View>
+    <ScrollView>
+      <Text style={{fontSize: 96}}>Scroll me plz</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 96}}>If you like</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 96}}>Scrolling down</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 96}}>What's the best</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 96}}>Framework around?</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 80}}>React Native</Text>
+    </ScrollView>
   );
 };
 
