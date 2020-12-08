ee2964634618ceff9f77773e612eb1c202fd4031
hitnei <hoangnhi050298@gmail.com>
2020-12-08 22:02:01 +0700

    list with FlatList

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,48 +1,57 @@
 import React from 'react';
-import {ScrollView, Image, Text} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64,
-};
-
-const App = () => {
+const MyClass = () => {
   return (
-    <ScrollView>
-      <Text style={{fontSize: 96}}>Scroll me plz</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>If you like</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>Scrolling down</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>What's the best</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>Framework around?</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 80}}>React Native</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      {/* no logic */}
+      <FlatList
+        data={[
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+          {name: 'Devin'},
+        ]}
+        renderItem={({item}) => {
+          return <Text style={[styles.item]}>{item.name}</Text>;
+        }}
+      />
+    </View>
   );
 };
 
-export default App;
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 0,
+  },
+  item: {
+    padding: 15,
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+  },
+});
+
+//make this component available to the app
+export default MyClass;
